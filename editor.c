f2f27ab99204b304ef0303ce327d7e58696b96a8
Ephraim Kunz <ephraimkunz@me.com>
2017-07-22 08:49:25 -0700

    Cursor wraps at beginning and end of the line

--- editor.c
+++ editor.c
@@ -170,14 +170,24 @@ void editorOpen(char* filename) {
 }
 
 void editorMoveCursor(int key) {
+    struct erow* row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
     case ARROW_LEFT:
         if (E.cx != 0) {
             E.cx--;
+        } else if (E.cy > 0) { // Move to end of previous line
+            E.cy --;
+            E.cx = E.row[E.cy].size;
         }
         break;
     case ARROW_RIGHT:
+        if (row && E.cx < row->size) {
             E.cx ++;
+        } else if (row && E.cx == row->size) {
+            E.cy ++;
+            E.cx = 0;
+        }
         break;
     case ARROW_UP:
         if (E.cy != 0) {
@@ -189,6 +199,12 @@ void editorMoveCursor(int key) {
             E.cy ++;
         }
         break;
+    }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
     }
 }
 
